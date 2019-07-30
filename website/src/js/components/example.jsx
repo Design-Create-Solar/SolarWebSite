7a7304c80a3449c32164b4f040ab565c27213792
ishnoor <ishnoor.singh.ext@telekom-digital.com>
2019-07-30 10:03:52 +0530

    example

---
+++ website/src/js/components/example.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class example extends Component {
+    render() {
+        return (
+            <div>
+                <h1>wirubdnvfrfneorivnrjvnf</h1>
+            </div>
+        )
+    }
+}
