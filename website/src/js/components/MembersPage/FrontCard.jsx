d9676f4f3897eec9e7362b9980e89ab0359aaa06
Jessica Ho <jho@Jessicas-MacBook-Pro-2.local>
2019-08-17 00:01:16 -0700

    finished initial draft of MembersPage

--- website/src/js/components/MembersPage/FrontCard.jsx
+++ website/src/js/components/MembersPage/FrontCard.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from "react"
+import {styled} from '@material-ui/styles'
+import * as constants from "../../constants"
 
-const usericon = require("../../../images/usericon.png");
+// const usericon = require("../../../images/usericon.png");
 class FrontCard extends Component {
     render() {
         return (
             <div className="card-side side-front">
-               <img src={usericon} style={{ padding: 20 }}/>
-               <h1>Name</h1>
+               <img src={this.props.image} style={{ margin: 20, borderRadius: "50%", boxShadow: "3px 5px 5px gray" }}/>
+               <Name>{this.props.name}</Name>
+               <Position>{this.props.position}</Position>
             </div>
         )
     }
 }
+
+const Name = styled("h2") ({
+    fontFamily: "Avenir Next",
+    color: constants.HOME_PAGE_DARK_TEXT_COLOR,
+    padding: "0 0 0 0",
+    margin: "5px 0 0 0", 
+    // borderStyle: "solid", 
+    // borderColor: "black"
+})
+
+const Position = styled("h3") ({
+    fontFamily: "Avenir Next",
+    color: constants.HOME_PAGE_DARK_TEXT_COLOR, 
+        // TODO: make this a lighter blue
+    // borderStyle: "solid", 
+    // borderColor: "black",
+    padding: "0 0 0 0",
+    margin: "0 0 0 0",
+    opacity: 0.4
+})
 
 export default FrontCard
