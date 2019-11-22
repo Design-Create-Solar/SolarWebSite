132f4e16777378aa514e1a09b5d95caca93f5c22
Ishnoor-Singh <ishnoors@gmail.com>
2019-11-21 18:34:06 -0800

    for production

--- website/src/index.js
+++ website/src/index.js
@@ -3,35 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import LandingPage from './js/components/LandingPage';
 import ProjectPage from './js/components/ProjectPage';
-import MembersPage from "./js/components/MembersPage/MembersPage";
+import MembersPage from './js/components/MembersPage/MembersPage';
 import * as serviceWorker from './serviceWorker';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-  } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import BottomBanner from './js/components/BottomBanner';
-import ScrollToTop from "./js/components/ScrollToTop"
+import ScrollToTop from './js/components/ScrollToTop';
 import SponsorsPage from './js/components/SponsorsPage/SponsorsPage';
-  
+import './futura/futur.ttf';
 
 ReactDOM.render(
-    <Router>
-        <div>
-            <ScrollToTop>
-                <Switch>
-                    <Route exact path="/" component={LandingPage} />
-                    <Route exact path="/home" component={LandingPage} />
-                    <Route path="/programs" component={ProjectPage} />
-                    <Route path="/team" component={MembersPage} />
-                    <Route path="/sponsors" component={SponsorsPage} />
-                </Switch>
-            </ScrollToTop>
-            <BottomBanner/>
-        </div>
-    </Router>
+	<Router>
+		<div>
+			<ScrollToTop>
+				<Switch>
+					<Route exact path='/' component={LandingPage} />
+					<Route exact path='/home' component={LandingPage} />
+					<Route path='/programs' component={ProjectPage} />
+					<Route path='/team' component={MembersPage} />
+					<Route path='/sponsors' component={SponsorsPage} />
+				</Switch>
+			</ScrollToTop>
+			<BottomBanner />
+		</div>
+	</Router>,
 
-    , document.getElementById('root'));
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- website/src/js/components/InfoArea.jsx
+++ website/src/js/components/InfoArea.jsx
@@ -1,42 +1,69 @@
-import React from "react";
-import { styled } from "@material-ui/styles";
-import { Typography, Box } from "@material-ui/core";
-import * as constants from "../constants";
-import "../../infoArea.css"
+import React from 'react';
+import { styled } from '@material-ui/styles';
+import { Typography, Box } from '@material-ui/core';
+import * as constants from '../constants';
+import '../../infoArea.css';
 import Slideshow from './Slideshow';
-import {Desktop, Default, Mobile} from '../constants';
-import SlideshowMobile from "./SlideShowMobile"
+import { Desktop, Default, Mobile } from '../constants';
+import SlideshowMobile from './SlideShowMobile';
 
 var color;
-const InfoArea = (props) => {
+const InfoArea = props => {
 	if (props.color === constants.HOME_PAGE_DARK_COLOR)
 		return (
 			<div>
 				<Desktop>
-					<ContainerDark flexDirection={props.align === "left" ? "row" : "row-reverse"} backgroundColor="red">
+					<ContainerDark
+						flexDirection={
+							props.align === 'left' ? 'row' : 'row-reverse'
+						}
+						backgroundColor='red'
+					>
 						<TextArea>
-							<h4 className="info-header">{props.header}</h4>
+							<h4 className='info-header'>{props.header}</h4>
 							<ActualText>{props.text}</ActualText>
 						</TextArea>
-						{props.images &&<Slideshow images={props.images}/>}
+						{props.images && <Slideshow images={props.images} />}
 					</ContainerDark>
 				</Desktop>
 				<Default>
-					<ContainerDark flexDirection="column" backgroundColor="red">
+					<ContainerDark flexDirection='column' backgroundColor='red'>
 						<TextArea>
-							<h4 className="info-header mobile-header">{props.header}</h4>
-							<ActualText style={{padding: "0 0 0 0", margin: "0 0 0 0"}}>{props.text}</ActualText>
+							<h4 className='info-header mobile-header'>
+								{props.header}
+							</h4>
+							<ActualText
+								style={{
+									padding: '0 0 0 0',
+									margin: '0 0 0 0'
+								}}
+							>
+								{props.text}
+							</ActualText>
 						</TextArea>
-							{props.images &&<SlideshowMobile images={props.images}/>}
+						{props.images && (
+							<SlideshowMobile images={props.images} />
+						)}
 					</ContainerDark>
 				</Default>
 				<Mobile>
-					<ContainerDark flexDirection="column" backgroundColor="red">
+					<ContainerDark flexDirection='column' backgroundColor='red'>
 						<TextArea>
-							<h4 className="info-header mobile-header">{props.header}</h4>
-							<ActualText style={{padding: "0 0 0 0", margin: "0 0 0 0"}}>{props.text}</ActualText>
+							<h4 className='info-header mobile-header'>
+								{props.header}
+							</h4>
+							<ActualText
+								style={{
+									padding: '0 0 0 0',
+									margin: '0 0 0 0'
+								}}
+							>
+								{props.text}
+							</ActualText>
 						</TextArea>
-							{props.images &&<SlideshowMobile images={props.images}/>}
+						{props.images && (
+							<SlideshowMobile images={props.images} />
+						)}
 					</ContainerDark>
 				</Mobile>
 			</div>
@@ -45,30 +72,63 @@ const InfoArea = (props) => {
 		return (
 			<div>
 				<Desktop>
-					<ContainerLight flexDirection={props.align === "left" ? "row" : "row-reverse"} backgroundColor="red">
+					<ContainerLight
+						flexDirection={
+							props.align === 'left' ? 'row' : 'row-reverse'
+						}
+						backgroundColor='red'
+					>
 						<TextArea>
-							<h4 className="info-header">{props.header}</h4>
+							<h4 className='info-header'>{props.header}</h4>
 							<ActualText>{props.text}</ActualText>
 						</TextArea>
-						{props.images &&<Slideshow images={props.images}/>}
+						{props.images && <Slideshow images={props.images} />}
 					</ContainerLight>
 				</Desktop>
 				<Default>
-					<ContainerLight flexDirection="column" backgroundColor="red">
+					<ContainerLight
+						flexDirection='column'
+						backgroundColor='red'
+					>
 						<TextArea>
-							<h4 className="info-header mobile-header">{props.header}</h4>
-							<ActualText style={{padding: "0 0 0 0", margin: "0 0 0 0"}}>{props.text}</ActualText>
+							<h4 className='info-header mobile-header'>
+								{props.header}
+							</h4>
+							<ActualText
+								style={{
+									padding: '0 0 0 0',
+									margin: '0 0 0 0'
+								}}
+							>
+								{props.text}
+							</ActualText>
 						</TextArea>
-							{props.images &&<SlideshowMobile images={props.images}/>}
+						{props.images && (
+							<SlideshowMobile images={props.images} />
+						)}
 					</ContainerLight>
 				</Default>
 				<Mobile>
-					<ContainerLight flexDirection="column" backgroundColor="red">
+					<ContainerLight
+						flexDirection='column'
+						backgroundColor='red'
+					>
 						<TextArea>
-							<h4 className="info-header mobile-header">{props.header}</h4>
-							<ActualText style={{padding: "0 0 0 0", margin: "0 0 0 0"}}>{props.text}</ActualText>
+							<h4 className='info-header mobile-header'>
+								{props.header}
+							</h4>
+							<ActualText
+								style={{
+									padding: '0 0 0 0',
+									margin: '0 0 0 0'
+								}}
+							>
+								{props.text}
+							</ActualText>
 						</TextArea>
-							{props.images &&<SlideshowMobile images={props.images}/>}
+						{props.images && (
+							<SlideshowMobile images={props.images} />
+						)}
 					</ContainerLight>
 				</Mobile>
 			</div>
@@ -76,46 +136,33 @@ const InfoArea = (props) => {
 };
 
 const ContainerDark = styled(Box)({
-	display: "flex",
-	alignItems: "center",
+	display: 'flex',
+	alignItems: 'center',
 	backgroundColor: constants.HOME_PAGE_DARK_COLOR,
 	color: constants.HOME_PAGE_LIGHT_TEXT_COLOR
 });
 
 const ContainerLight = styled(Box)({
-	display: "flex",
-	alignItems: "center",
+	display: 'flex',
+	alignItems: 'center',
 	backgroundColor: constants.HOME_PAGE_LIGHT_COLOR,
 	color: constants.HOME_PAGE_DARK_TEXT_COLOR
 });
 
 const TextArea = styled(Box)({
 	order: 0,
-	display: "flex",
-	flexDirection: "column",
-	width: "50%",
-	paddingTop: "5%",
-	paddingBottom: "5%",
-	paddingRight: "3%",
-	paddingLeft: "3%",
+	display: 'flex',
+	flexDirection: 'column',
+	width: '50%',
+	paddingTop: '5%',
+	paddingBottom: '5%',
+	paddingRight: '3%',
+	paddingLeft: '3%'
 });
 
-const ActualText = styled('p') ({
-	fontFamily: "Futura", 
-	fontSize: "25px"
+const ActualText = styled('p')({
+	fontFamily: 'Futura',
+	fontSize: '25px'
 });
-
-<<<<<<< HEAD
-{/* <Responsive displayIn={["Laptop"]}>
-<ContainerDark flexDirection={props.align === "left" ? "row" : "row-reverse"} backgroundColor="red">
-	<TextArea>
-		<h4 className="info-header">{props.header}</h4>
-		<Typography variant="body1">{props.text}</Typography>
-	</TextArea>
-	{props.images &&<Slideshow images={props.images}/>}
-</ContainerDark>
-</Responsive> */}
-=======
->>>>>>> responsive info area, apt pics, right meeting times
 
 export default InfoArea;
