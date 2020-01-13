2c016fa74ec6ce3d075787dfaaa35b2b122075b5
Ishnoor-Singh <ishnoors@gmail.com>
2020-01-12 16:29:03 -0800

    for newjessica branch

---
+++ website/backend/server.js
@@ -0,0 +1,16 @@
+
+const Influx = require('influxdb-nodejs')
+//const client = new Influx('http://127.0.0.1:8086/testdb') //no auth
+const express = require("express")
+const bodyParser = require("body-parser")
+const dataRoutes = require("./routes/data")
+const app = express()
+const PORT = 5000
+app.use(bodyParser.json())
+app.use("/data", dataRoutes)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.listen(PORT, () => {
+    console.log("Listening on " + PORT)
+})
+
+
