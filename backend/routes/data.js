f25333ba4ddbdf3d6f5c66bf754c9534da99dbdf
Yarmoski <ted@yarmoski.com>
2020-01-21 18:40:14 -0800

    Revise number, time GET routes

--- backend/routes/data.js
+++ backend/routes/data.js
@@ -1,5 +1,13 @@
 const router = require("express").Router();
 const client = require("../config/influx.js");
+const socketIO = require("socket.io");
+const sockettest = async (socket, data) => {
+  try {
+    socket.emit("FromAPI", data)
+  } catch (error) {
+    console.error('Error ${error.code}')
+  }
+}
 
 router.post("/add", (req, res) => {
   let sID = req.body.sID;
@@ -15,7 +23,10 @@ router.post("/add", (req, res) => {
       temperature: tempValue,
       measureDevice: deviceString
     })
-    .then(() => console.info("write point success"))
+    .then(() => {
+      sockettest("teststring")
+      console.info("write point success")
+    })
     .catch(console.error);
 
   res.json({
@@ -65,7 +76,7 @@ router.post("/addAcc", (req, res) => {
   });
 });
 
-router.get("/query/all/:type", (req, res) => {
+router.get("/query/:type/all", (req, res) => {
   let type = req.params.type;
   let reader = client.query(type);
   reader
@@ -77,7 +88,7 @@ router.get("/query/all/:type", (req, res) => {
     });
 });
 
-router.get("/query/entries/:type/:number", (req, res) => {
+router.get("/query/:type/:number", (req, res) => {
   let type = req.params.type;
   let number = req.params.number;
   let reader = client.query(type);
@@ -92,11 +103,13 @@ router.get("/query/entries/:type/:number", (req, res) => {
     });
 });
 
-router.get("/query/time/:type/:time", (req, res) => {
+router.get("/query/:type/start/:stime/end/:etime", (req, res) => {
   let type = req.params.type;
   let reader = client.query(type);
-  let time = req.params.time;
-  reader.start = "-" + time + "h";
+  let stime = req.params.stime;
+  let etime = req.params.etime;
+  reader.end = "-" + etime + "h";
+  reader.start = "-" + stime + "h";
   reader
     .then(data => {
       res.send(JSON.stringify(data));
