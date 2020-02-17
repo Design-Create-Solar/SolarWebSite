cd39266cba814e6cf4658ce53cccb7ba2c40d31e
Jessica Ho <jho@Jessicas-MacBook-Pro-2.local>
2020-02-16 17:42:27 -0800

    god kill me 5

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,14 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const dataRoutes = require("./routes/data")
 const app = express()
-const PORT = 5000
+const PORT = 5001
 app.use(bodyParser.json())
 app.use("/data", dataRoutes)
+
+app.use('/static', express.static('media'))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.listen(PORT, () => {
     console.log("Listening on " + PORT)
 })
-
 
