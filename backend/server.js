78f35f2e34548ce51a5f67407690f7da5b4c2113
Jessica Ho <jho@Jessicas-MacBook-Pro-2.local>
2020-02-16 17:45:21 -0800

    do you wanna build a snowman

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const dataRoutes = require("./routes/data")
 const app = express()
-const PORT = 5001
+const PORT = 5000
 app.use(bodyParser.json())
 app.use("/data", dataRoutes)
 
