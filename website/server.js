ac638434e96d18386341889eb4d458fa6cac6d43
Jessica Ho <jessicaho@Jessicas-MacBook-Pro-3.local>
2020-06-19 03:17:33 -0700

    new docker config

---
+++ website/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use('/', express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+        console.log('hi');
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(PORT, () => {
+        console.log('app now listening for requests on port ' + PORT);
+});
