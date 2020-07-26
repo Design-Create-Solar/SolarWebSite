7a96951995853bf5176d8ef50db9515780432c87
Jessica Ho <jessicaho@jessicas-mbp-3.attlocal.net>
2020-07-26 02:31:00 -0700

    fix server + docker

--- website/server.js
+++ website/server.js
@@ -3,11 +3,11 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-// app.use('/', express.static(path.join(__dirname, 'build')));
+app.use("/", express.static(path.join(__dirname, "build")));
 
-// app.get('/', function (req, res) {
-//         console.log('hi');
-//         res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// app.get("/", function (req, res) {
+//   console.log("hi");
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
 // });
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname + "/build/index.html"));
