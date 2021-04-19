38b5df1c2d4d641dfe964b8c65bb15b4e85e459c
Jessica Ho <hojessica1230@gmail.com>
2021-04-18 23:44:04 -0700

    initialFiles fix

--- website/src/context/BlocksContext.js
+++ website/src/context/BlocksContext.js
@@ -2,10 +2,9 @@ import React, { useState, useEffect } from "react"
 // import * as constants from "../js/components/MultiplePages/constants"
 
 const BlocksContext = React.createContext()
-
+let imgs = []
 function BlocksProvider(props) {
     const [blocks, setBlocks] = useState([]);
-
     useEffect(() => {
         let mounted = true
         fetch("http://localhost:5000/block/getBlocks")
@@ -16,8 +15,20 @@ function BlocksProvider(props) {
         return () => mounted = false
     }, [])
 
+    useEffect(() => {
+        let mounted = true
+        for (const block of blocks) {
+            let tmp = []
+            for (const img of block.images) {
+                tmp.push("https://elasticbeanstalk-us-west-2-127661128201.s3-us-west-2.amazonaws.com/" + img)
+            }
+            imgs.push(tmp)
+        }
+        return () => mounted = false
+    }, [blocks])
+
     return (
-        <BlocksContext.Provider value={{ blocks, setBlocks }}>{props.children}</BlocksContext.Provider>
+        <BlocksContext.Provider value={{ blocks, setBlocks, imgs }}>{props.children}</BlocksContext.Provider>
     );
 }
 
