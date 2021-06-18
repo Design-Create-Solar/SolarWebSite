7e43db3192137f60a07fdacd67186065c4b26dac
Jessica Ho <hojessica1230@gmail.com>
2021-06-17 17:58:12 -0700

    rewrite influx with new library

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,11 @@ const dotenv = require('dotenv').config();
 const fileUpload = require('express-fileupload');
 
 //Databases
-const Influx = require('influxdb-nodejs');
 const mongoose = require('mongoose');
 
 const express = require('express'),
 	app = express(),
 	PORT = 5000;
-const bodyParser = require('body-parser');
 const cors = require('cors');
 app.use(cors());
 
@@ -49,29 +47,3 @@ mongoose.connect(
 );
 
 app.listen(PORT, () => console.log('Server listening on port ' + PORT + '.'));
-
-//socket.io realtime updates:
-
-//const socketIo = require("socket.io");
-
-// const server = require("http").Server(app);
-// const io = socketIo(server);
-// server.listen(PORT, () => {
-//   console.log("Listening on " + PORT);
-// });
-
-// let interval;
-// io.on("connection", (socket) => {
-//   console.log("New client connected");
-//   module.exports = socket;
-//   if (interval) {
-//     clearInterval(interval);
-//   }
-//   interval = setInterval(
-//     () => /* insert get request here? */ console.log("still connected"),
-//     10000
-//   );
-//   socket.on("disconnect", () => {
-//     console.log("Client disconnected");
-//   });
-// });
